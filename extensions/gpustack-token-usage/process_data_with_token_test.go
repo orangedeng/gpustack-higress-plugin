bbb3aa98e157842feea8fb0138ad46618ed8faae
Yuxing Deng <jxfa0043379@hotmail.com>
2025-12-24 22:48:36 +0800

    fix: usage doesn't set correctly in mindIE upstream
    If a large usage response is sent to onStreamingResponseBody function,
    it will be seperated into multiple frame. It needs to be assembled into a valid json
    before modifying the usage data.

--- extensions/gpustack-token-usage/process_data_with_token_test.go
+++ extensions/gpustack-token-usage/process_data_with_token_test.go
@@ -10,10 +10,14 @@ func TestProcessDataWithToken(t *testing.T) {
 	// 构造一个简单的 JSON 响应体
 	origin := `data: {"id":"chatcmpl-2c66674e-719d-4be4-a0f1-d7cdbd76df30","object":"chat.completion.chunk","created":1762490796,"model":"qwen3-0.6b","choices":[],"usage":{"prompt_tokens":132,"total_tokens":311,"completion_tokens":179}}`
 	ttft := int64(123)
-	tpot := float64(45.4555)
-	tps := float64(6.6677)
+	tpot := float64(45.46)
+	tps := float64(6.67)
 
-	result := process_data_with_token([]byte(origin), ttft, tpot, tps)
+	result := process_data_with_token([]byte(origin), map[string]any{
+		"time_to_first_token_ms":   ttft,
+		"time_per_output_token_ms": tpot,
+		"tokens_per_second":        tps,
+	})
 
 	var m map[string]interface{}
 	err := json.Unmarshal(result, &m)
